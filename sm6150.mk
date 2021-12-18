2f6171b49bd044c621fdabc7ee3ce08b0d86614c
Erfan Abdi <erfangplus@gmail.com>
2021-12-18 07:37:10 +0330

    sm6150-common: Add hotword priv app permissions
    Change-Id: Ic936e852cb77fe47c0ce69465b7a3717c2619b7e

--- sm6150.mk
+++ sm6150.mk
@@ -204,6 +204,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
