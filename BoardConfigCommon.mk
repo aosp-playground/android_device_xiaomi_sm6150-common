2c13082ea3180e763ad7eaca14e8e6e76edfa0ae
basamaryan <basam.aryan@gmail.com>
2022-04-22 13:16:45 -0400

    sm6150-common: Drop Lineage trust HAL
    Change-Id: Ic4f467439e64d156bedb386b80690b2bafe5ae0a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,11 +163,6 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
-# Trust
-TARGET_TRUST_USB_CONTROL_PATH := /sys/devices/platform/soc/a600000.ssusb/usb_data_enabled
-TARGET_TRUST_USB_CONTROL_ENABLE := 0
-TARGET_TRUST_USB_CONTROL_DISABLE := 1
-
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
