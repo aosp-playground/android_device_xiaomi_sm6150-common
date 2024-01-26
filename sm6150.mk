41a9b414625a300ae5c6dba342fd7ce2c5e2170f
basamaryan <basam.aryan@gmail.com>
2024-01-26 16:58:23 -0500

    sm6150-common: Use common vibrator component
    Change-Id: I6217bc1245d272f85cbfa1a7d5847bea37fee4c1

--- sm6150.mk
+++ sm6150.mk
@@ -263,6 +263,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     display \
     perf \
     telephony \
+    vibrator \
     wfd
 
 PRODUCT_COPY_FILES += \
@@ -306,13 +307,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vulkan
 PRODUCT_PACKAGES += \
