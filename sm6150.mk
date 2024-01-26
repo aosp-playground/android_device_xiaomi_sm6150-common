48740a337fbfbd8e408b0a2b0b5a2de1d1564373
basamaryan <basam.aryan@gmail.com>
2024-01-26 16:51:28 -0500

    sm6150-common: Use common wfd component
    Change-Id: I7d5cad2727c29ca4d62daa901a0f25a6a1cd220d

--- sm6150.mk
+++ sm6150.mk
@@ -261,7 +261,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     display \
     perf \
-    telephony
+    telephony \
+    wfd
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml
@@ -339,14 +340,8 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libavservices_minijail_32 \
     libdisplayconfig.qti \
-    libnl \
-    libqdMetaData \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+    libqdMetaData
 
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
