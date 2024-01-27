df28feb4daecd2b162fa91b648f1e5f22c996d73
basamaryan <basam.aryan@gmail.com>
2024-01-27 17:10:24 -0500

    sm6150-common: Add broken rules
    Change-Id: I71e1ca4e76f17dbe6b28494609ea5a43d2fe64d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,9 @@ BOARD_VENDOR := xiaomi
 
 COMMON_PATH := device/xiaomi/sm6150-common
 
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Inherit from proprietary files
 include vendor/xiaomi/sm6150-common/BoardConfigVendor.mk
 
